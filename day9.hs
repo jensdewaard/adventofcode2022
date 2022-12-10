5053d006be0d3e7e8ee3a11253037fd6a38743c4
Jens de Waard <jcdewaard@protonmail.com>
2022-12-10 13:11:29 +0100

    Solve day 9 part 2

--- day9.hs
+++ day9.hs
@@ -7,44 +7,48 @@ import Data.List
 main :: IO ()
 main = do
     contents <- readFile "inputs/9.txt"
-    let ms = filter (/= "") $ map T.unpack (T.splitOn (T.pack "\n") (T.pack contents))
-    let h = (0,0)
-    let t = Tail (0,0) []
-    let (h', Tail t' ts) = foldl moveHead (h, t) (map readMove ms)
-    -- 239 is too low
-    -- 6265 is too low
-    print $ length $ nub (t' : ts)
+    let ms = concatMap readMove $ filter (/= "") $ map T.unpack (T.splitOn (T.pack "\n") (T.pack contents))
+    let rope = ((0,0), [(0,0)])
+    let tenKnotRope = ((0,0), replicate 9 (0,0))
+    let ps = scanl doMove rope ms
+    let ps' = scanl doMove tenKnotRope ms
+    print $ length $ nub $ map (last. snd) ps
+    print $ length $ nub $ map (last . snd) ps'
+
 
 data Dir = Up | L | R | Down deriving (Show)
 type Move = (Dir, Int)
+type Rope = ((Int, Int), [(Int, Int)])
 
 type Head = (Int, Int)
-data Tail = Tail (Int,Int) [(Int, Int)] deriving (Show)
+type Tail = [(Int,Int)]
 
-readMove :: String -> Move
-readMove ('U' : s) = (Up, read s)
-readMove ('L' : s) = (L, read s)
-readMove ('R' : s) = (R, read s)
-readMove ('D' : s) = (Down, read s)
+readMove :: String -> [Move]
+readMove ('U' : s) = replicate (read s) (Up, 1)
+readMove ('L' : s) = replicate (read s) (L, 1)
+readMove ('R' : s) = replicate (read s) (R, 1)
+readMove ('D' : s) = replicate (read s) (Down, 1)
 
-moveHead :: (Head,Tail) -> Move -> (Head, Tail)
-moveHead (h,t) (_, 0) = (h, t)
-moveHead (h,t) (Up, n) = moveHead (h', moveTail h h' t) (Up, n-1)
-    where h' = (fst h, snd h - 1)
-moveHead (h, t) (Down, n) = moveHead (h', moveTail h h' t) (Down, n-1)
-    where h' = (fst h, snd h + 1)
-moveHead (h, t) (L, n) = moveHead (h', moveTail h h' t) (L, n-1)
-    where h' = (fst h - 1, snd h)
-moveHead (h, t) (R, n) = moveHead (h', moveTail h h' t) (R, n-1)
-    where h' = (fst h + 1, snd h)
+moveHead :: (Int, Int) -> Move -> (Int, Int)
+moveHead (x, y) (Up, n) = (x, y - n)
+moveHead (x, y) (Down, n) = (x, y + n)
+moveHead (x, y) (L, n) = (x - n, y)
+moveHead (x, y) (R, n) = (x + n, y)
 
+doMove :: Rope -> Move -> Rope
+doMove (h , t) m = (h', drop 1 $ moveRope (h': t))
+    where h' = moveHead h m
 
-moveTail :: 
-    Head {- current head position -}
-    -> Head {- new head position -}
-    -> Tail {- current tail position -}
-    -> Tail {- new tail position -}
-moveTail h h' (Tail t ts) = if distance h' t > 1 then Tail h (t: ts) else Tail t ts
+moveRope :: [(Int, Int)] -> [(Int, Int)]
+moveRope [] = []
+moveRope [t] = [t]
+moveRope (t: t' : ts) = if distance t t' > 1
+    then t : t'' : drop 1 (moveRope (t'' : ts))
+    else t : t' : ts
+    where t'' = moveToward t t'
+
+moveToward :: (Int, Int) -> (Int, Int) -> (Int, Int)
+moveToward p t = (fst t + signum (fst p - fst t), snd t + signum (snd p - snd t))
 
 distance :: (Int, Int) -> (Int, Int) -> Int
 distance (x, y) (x', y') = max (abs (x' - x)) (abs (y' - y))
